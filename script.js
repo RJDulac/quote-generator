8f851654528879d4184abd795b93a2a173b6847e
machine <analyticrye@gmail.com>
2020-09-04 02:22:16 -0400

    grabbed quotes from api and tweeting functionality

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//grab Ids from dom
+const quoteContainer = document.getElementById("quote-container"),
+  quoteText = document.getElementById("quote"),
+  authorText = document.getElementById("author"),
+  twitterButton = document.getElementById("twitter"),
+  newQuoteButton = document.getElementById("new-quote");
+
 //Get quote from API
 
 const getQuote = async () => {
@@ -8,13 +15,37 @@ const getQuote = async () => {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    //if author is blank, assign "Unkown"
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unkown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    //Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     //api is using special characters that causes an error
     getQuote();
-    console.log("Something went wrong", error);
   }
 };
+
+//tweet quote
+const tweetQuote = () => {
+  const quote = quoteText.innerText,
+    author = authorText.innerText,
+    twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  //open new window
+  open(twitterUrl, "_blank");
+};
+
+//Event Listeners
+newQuoteButton.addEventListener("click", getQuote);
+twitterButton.addEventListener("click", tweetQuote);
 
 //On load
 getQuote();
